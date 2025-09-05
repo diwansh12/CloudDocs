093fade942678636bee813dcc2a5729ca4f15781
diwansh12 <diwansh1112@gmail.com>
2025-09-06 02:42:15 +0530

    Update DocumentDetails.tsx

--- clouddocs-frontend/clouddocs-frontend/src/pages/DocumentDetails.tsx
+++ clouddocs-frontend/clouddocs-frontend/src/pages/DocumentDetails.tsx
@@ -8,7 +8,6 @@ import Sidebar from '../components/layout/Sidebar';
 import ShareModal from '../components/ShareModal';
 import EditMetadataModal from '../components/EditMetadataModal';
 import documentService, { Document } from '../services/documentService';
-import TimestampDebug from '../components/TimeStampDebug';
 
 export default function DocumentDetails() {
   const { documentId } = useParams<{ documentId: string }>();
@@ -332,10 +331,6 @@ export default function DocumentDetails() {
             </Button>
           </div>
         </section>
-
-        {documentId && (
-          <TimestampDebug workflowId={documentId} />
-        )}
       </main>
 
       {document && (
